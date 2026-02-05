ea97fa3e9f8b39d6b5440acd945a1a8e1f969a7e
aasimzia <aasimziaawan@gmail.com>
2026-02-05 15:57:38 +0500

    update helloworld recipe

--- recipes-example/helloworld/files/helloworld.c
+++ recipes-example/helloworld/files/helloworld.c
@@ -1,6 +1,14 @@
 #include "stdio.h"
+#include "helloworld.h"
 
 int main() {
+    
+    #if HELLO == y
     printf("Hello, World!\n");
+    #endif
+    
+    #if WELCOME == y
+    printf(|"Welcome to this world\n")
+    #endif
     return 0;
 }
